b773f516d749ccf9a63a06a06f13096ab4f535cf
seungjin <jinybear@nuricon.co.kr>
2023-07-07 23:25:20 -0700

    middleware 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { typeORMConfig } from './config/typeorm.config';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+
 
 @Module({
   imports: [        
@@ -13,4 +15,13 @@ import { typeORMConfig } from './config/typeorm.config';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+
+export class AppModule implements NestModule { 
+  configure(consumer: MiddlewareConsumer) {
+      //consumer.apply(LoggerMiddleware).forRoutes({path: 'parking/auth/user', method: RequestMethod.GET})
+      consumer
+        .apply(LoggerMiddleware)
+        //.exclude({path: 'parking/auth/user', method: RequestMethod.GET})
+        .forRoutes('parking/auth/user')
+  }
+}
